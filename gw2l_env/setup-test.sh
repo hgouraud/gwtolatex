49a10ae679973e3f2516dbc1fa4e6a8cb59ec494
Henri83 <henri.gouraud@laposte.net>
2024-01-06 11:50:25 +0100

    bump version number

--- gw2l_env/setup-test.sh
+++ gw2l_env/setup-test.sh
@@ -10,3 +10,8 @@ cp -R ./src ../bases
 cp -R ./tex ../bases/etc
 
 ../../geneweb/distribution/gw/gwc -f -o ../bases/base-test base-test.gw
+if [ $? -eq 0 ]; then
+    echo "bases folder initialized and base-test created"
+else
+    echo "GeneWeb must be installed and launched. See README"
+fi
